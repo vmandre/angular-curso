f7f5ac751741826047dec970be5b4a6700d7890d
Vanderlei Matos <vmandre@gmail.com>
2016-10-07 12:59:08 -0300

    HTTP [#5] Getting Started with AngularJS (Free Tutorial)

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -2,5 +2,11 @@ angular
 	.module('ngCribs')
 	.controller('cribsController', function($scope, cribsFactory){
 		
-		$scope.cribs = cribsFactory.getCribs();
+		$scope.cribs;
+
+		cribsFactory.getCribs().success(function(data) {
+			$scope.cribs = data;
+		}).error(function(error) {
+			console.log(error);
+		});
 	});
